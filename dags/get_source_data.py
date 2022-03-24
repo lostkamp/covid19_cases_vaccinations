563355e5e3fb6ffdf878edbaf1d663c76b213b38
Lutz Ostkamp <35694434+lostkamp@users.noreply.github.com>
2022-03-24 21:24:12 +0100

    Add tasks for vaccination data

--- dags/get_source_data.py
+++ dags/get_source_data.py
@@ -9,7 +9,8 @@ from resources.download import (get_case_datafile_url, download_case_file,
 
 conf = {
     'repo_url_case': 'https://storage.googleapis.com/brdata-public-data/rki-corona-archiv/2_parsed/index.html',  # NOQA
-    'repo_url_vaccinations': '',
+    'repo_url_vaccinations': 'https://github.com/robert-koch-institut/COVID-19-Impfungen_in_Deutschland/tree/master/Archiv/',
+    'filename_template_vaccinations': '{DATE}_Deutschland_Landkreise_COVID-19-Impfungen.csv?raw=true',
     'bucket_name': 'udacity-dend-capstone-lostkamp',
     's3_prefix_case': 'case_data',
     's3_prefix_vaccinations': 'vaccination_data'
@@ -31,10 +32,13 @@ with DAG(dag_id='get_source_data',
          concurrency=15,
          catchup=False) as dag:
 
+    date = '2021-07-23'
+
     get_case_url_task = PythonOperator(
         task_id='get_case_url_task',
         python_callable=get_case_datafile_url,
-        op_kwargs={'repo_url': conf['repo_url_case']}
+        op_kwargs={'date': date,
+                   'repo_url': conf['repo_url_case']}
     )
     download_case_file_task = PythonOperator(
         task_id='download_case_file_task',
@@ -50,6 +54,20 @@ with DAG(dag_id='get_source_data',
         op_kwargs={'s3_prefix': conf['s3_prefix_case'],
                    'bucket_name': conf['bucket_name']}
     )
+    filename = conf['filename_template_vaccinations'].format(DATE=date)
+    download_vaccination_file_task = BashOperator(
+        task_id='download_vaccination_file_task',
+        bash_command='curl -L $URL > $LOCAL_FNAME && echo $LOCAL_FNAME | cut -d "?" -f 1',
+        env={'URL': conf['repo_url_vaccinations'] + filename,
+             'LOCAL_FNAME': filename}
+    )
+    upload_vaccination_file_to_s3_task = PythonOperator(
+        task_id='upload_vaccination_file_to_s3_task',
+        python_callable=upload_file_to_s3,
+        op_kwargs={'s3_prefix': conf['s3_prefix_vaccinations'],
+                   'bucket_name': conf['bucket_name']}
+    )
 
 
 get_case_url_task >> download_case_file_task >> decompress_case_file_task >> upload_case_file_to_s3_task
+download_vaccination_file_task >> upload_vaccination_file_to_s3_task
--- dags/resources/download.py
+++ dags/resources/download.py
@@ -8,16 +8,16 @@ from pathlib import Path
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 
-def get_case_datafile_url(ds, repo_url):
+def get_case_datafile_url(date, repo_url):
     from bs4 import BeautifulSoup
-    logging.info(f'Getting data URL for: {ds}')
+    logging.info(f'Getting data URL for: {date}')
     urlretrieve(repo_url, 'index.html')
     with open('index.html', 'r') as f:
         soup = BeautifulSoup(f, 'html.parser')
     links_tags = soup.find_all('a')
     file_urls = list(map(lambda x: x.attrs['href'], links_tags))
 
-    result_list = list(filter(lambda x: ds in x, file_urls))
+    result_list = list(filter(lambda x: date in x, file_urls))
     if len(result_list) > 1:
         # More than one dataset is available for the requested date.
         # In that case we take the one with the largest timestamp
@@ -25,7 +25,7 @@ def get_case_datafile_url(ds, repo_url):
     elif len(result_list) == 1:
         result = result_list[0]
     else:
-        raise IOError(f'No file found matching {ds}')
+        raise IOError(f'No file found matching {date}')
 
     logging.info(f'Found URL: {result}')
     return result
@@ -40,22 +40,27 @@ def download_case_file(ti):
     return local_fname
 
 
-def decompress_case_file(ti):
+def decompress_case_file(ti, delete_compressed_file=True):
     filename_in = ti.xcom_pull(task_ids='download_case_file_task')
     filename_out = os.path.splitext(filename_in)[0]
     with lzma.open(filename_in, 'rb') as fin, open(filename_out, 'wb') as fout:
         shutil.copyfileobj(fin, fout)
-    logging.info(f'Decompressed file: {filename_out}')
+    logging.info(f'Decompressed file: {os.path.join(os.getcwd(), filename_out)}')
+    if delete_compressed_file:
+        logging.info(f'Deleting compressed file: {os.path.join(os.getcwd(), filename_in)}')
+        Path(filename_in).unlink()
     return filename_out
 
 
 def upload_file_to_s3(ti, s3_prefix, bucket_name, delete_local_file=True):
-    local_fname = ti.xcom_pull(task_ids='decompress_case_file_task')
+    local_fname = ti.xcom_pull(task_ids=['decompress_case_file_task',
+                                         'download_vaccination_file_task'])
+    logging.info(f'Local filename: {local_fname}')
     s3 = S3Hook(aws_conn_id='aws_credentials', region_name='eu-west-1')
     s3_key = os.path.join(s3_prefix, local_fname)
     logging.info(f'Uploading to S3 path: s3://{bucket_name}/{s3_key}')
     s3.load_file(local_fname, key=s3_key, bucket_name=bucket_name)
     logging.info('Upload complete.')
     if delete_local_file:
-        logging.info(f'Deleting local file: {local_fname}')
+        logging.info(f'Deleting local file: {os.path.join(os.getcwd(), local_fname)}')
         Path(local_fname).unlink()
